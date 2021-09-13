49a8e4c6604eaaaa74902b0e9d594d27c190968f
TingedLicense <90207392+TingedLicense@users.noreply.github.com>
2021-09-13 19:27:58 +0300

    Изучали иеархию

---
+++ RubicTest/RubicTest/House.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubicTest
+{
+    class House
+    {
+        private bool isFlat = true;
+        private string street = "Пролетарская";
+        private int floor = 26;
+        private float prise = 4500000F;
+
+    }
+}
